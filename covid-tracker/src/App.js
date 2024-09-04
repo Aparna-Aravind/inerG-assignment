a7b4af8497fa294e606e40c89547860251c0d12e
Aparna <workwithaparna1997@gmail.com>
2024-09-04 14:37:10 +0530

    Final

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import FilterData from "./Components/FilterData/FilterData";
+import LineChart from "./Components/LineChart/LineChart";
+import MapView from "./Components/MapView/MapView";
+import PieChart from "./Components/PieChart/PieChart";
 
 function App() {
   return (
     <div className="container">
       <FilterData />
+      <PieChart/>
+      <LineChart/>
+      <MapView/>
     </div>
   );
 }
